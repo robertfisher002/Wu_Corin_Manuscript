6391876f1a7db1beb4508269732146a3f8a9ccae
robertfisher002 <90728516+robertfisher002@users.noreply.github.com>
2024-01-02 10:26:26 -0500

    Delete Figure_6 directory

--- Figure_6/sample_meta_M10_R4.r
+++
@@ -1,6 +0,0 @@
-library("ChIPseqSpikeInFree")
-metaFile <- "sample_meta_M10_R4.txt"
-bams <- c("M10-R_corin_H3K27ac_R4.mappedq1.sort.rmdup.bam","M10-R_corin_H3K4me2_R4.mappedq1.sort.rmdup.bam","M10-R_corin_H3K9ac_R4.mappedq1.sort.rmdup.bam","M10-R_corin_Input_R4.mappedq1.sort.rmdup.bam","M10-R_DMSO_H3K27ac_R4.mappedq1.sort.rmdup.bam","M10-R_DMSO_H3K4me2_R4.mappedq1.sort.rmdup.bam","M10-R_DMSO_H3K9ac_R4.mappedq1.sort.rmdup.bam","M10-R_DMSO_Input_R4.mappedq1.sort.rmdup.bam")
-ChIPseqSpikeInFree(bamFiles = bams, chromFile = "hg38", metaFile = metaFile, prefix = "M10_R4_rmdup")
-
-
--- Figure_6/sample_meta_M13_R1.r
+++
@@ -1,6 +0,0 @@
-library("ChIPseqSpikeInFree")
-metaFile <- "sample_meta_M13_R1.txt"
-bams <- c("M13-R_corin_H3K27ac_R1.mappedq1.sort.rmdup.bam","M13-R_corin_H3K4me2_R1.mappedq1.sort.rmdup.bam","M13-R_corin_H3K9ac_R1.mappedq1.sort.rmdup.bam","M13-R_corin_Input_R1.mappedq1.sort.rmdup.bam","M13-R_DMSO_H3K27ac_R1.mappedq1.sort.rmdup.bam","M13-R_DMSO_H3K4me2_R1.mappedq1.sort.rmdup.bam","M13-R_DMSO_H3K9ac_R1.mappedq1.sort.rmdup.bam","M13-R_DMSO_Input_R1.mappedq1.sort.rmdup.bam")
-ChIPseqSpikeInFree(bamFiles = bams, chromFile = "hg38", metaFile = metaFile, prefix = "M13_R1_rmdup")
-
-
